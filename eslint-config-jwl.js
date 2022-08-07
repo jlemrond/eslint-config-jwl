a581df3b64a8fa30b612121ffff17f8029eee36f
Jason Lemrond <jason.lemrond@icloud.com>
2022-08-07 12:30:51 -0400

    Add prettier to eslint config

--- eslint-config-jwl.js
+++ eslint-config-jwl.js
@@ -1,15 +1,13 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    plugins: [
-        '@typescript-eslint'
-    ],
-    env: {
-        es2021: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-}
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+};
